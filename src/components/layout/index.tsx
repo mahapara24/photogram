a20e9aa46a40a4c5fba94e1d9f033191152203f6
Mahapara Nizamani <mahapara2424@gmail.com>
2024-07-19 10:54:32 +0500

    added

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -9,11 +9,11 @@ interface ILayoutProps {
 const Layout: React.FunctionComponent<ILayoutProps> = ({ children }) => {
   return (
     <div className="flex bg-white">
-      <aside className="flex gap-x-4 bg-gray-800 fixed top-0 z-40 lg:w-60 h-screen">
+      <aside className="flex gap-x-4 bg-gray-800 fixed top-0 left-0 z-40 lg:w-60 h-screen">
         <Sidebar />
       </aside>
       <div className="lg:ml-60 lg:mr-60 p-8 flex-1 ml-36">{children}</div>
-      <aside className="bg-gray-800 fixed top-0 z-40 lg:w-60 h-screen hidden lg:block">
+      <aside className="bg-gray-800 fixed top-0 right-0 z-40 lg:w-60 h-screen hidden lg:block">
         <UserList />
       </aside>
     </div>
