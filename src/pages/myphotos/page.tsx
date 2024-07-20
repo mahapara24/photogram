bab8f8aaaa0a683e69d59c4e7eb49537bf0c2e93
Mahapara Nizamani <mahapara2424@gmail.com>
2024-07-20 19:21:53 +0500

    side bar added to all pages

--- src/pages/myphotos/page.tsx
+++ src/pages/myphotos/page.tsx
@@ -1,12 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Layout from "../../components/layout";
 
 interface IMyPhotosProps {}
 
 const MyPhotos: React.FunctionComponent<IMyPhotosProps> = () => {
+  const [count, setCount] = useState(0);
+  useEffect(() => {
+    console.log("Component Rendered succesfully ");
+  }, []);
+
+  const btnClick = () => {
+    setCount(count + 1);
+    setCount(count + 1);
+
+    setCount(count + 1);
+  };
   return (
     <Layout>
       <div>MyPhotos</div>
+      <div>
+        <h1>interview preparation code practice</h1>
+        <button onClick={btnClick}>Click me</button>
+        <p>you clicked {count} times </p>
+      </div>
     </Layout>
   );
 };
--- src/pages/post/page.tsx
+++ src/pages/post/page.tsx
@@ -6,7 +6,34 @@ interface IPostProps {}
 const Post: React.FunctionComponent<IPostProps> = () => {
   return (
     <Layout>
-      <div>Post</div>
+      <div className="flex justify-center">
+        <div className="border max-w-3xl w-full ">
+          <h3 className="bg-slate-800 text-white text-center  text-lg p-2 ">
+            Create Post
+          </h3>
+          <div className="p-8">
+            <form action="">
+              <div className="flex flex-col">
+                <label className="mb-4" htmlFor="caption">
+                  Photo Caption
+                </label>
+                <textarea
+                  className="mb-8"
+                  name=""
+                  placeholder="What's in your photo!"
+                  id="caption"
+                ></textarea>
+              </div>{" "}
+              <div className="flex flex-col">
+                <label htmlFor="photo" className="mb-4">
+                  Photos
+                </label>
+              </div>
+              <button></button>
+            </form>
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
